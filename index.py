8521726e1817613b571f82310fc0b3fc64654d7e
Paul-Kim <kmc5500@naver.com>
2015-12-26 10:52:14 +0900

    add to ignore (.idea) python project file

--- index.py
+++ index.py
@@ -1,10 +1,18 @@
 from flask import Flask
+from flask.ext.mysqldb import MySQL
+
 app = Flask(__name__)
+app.config['MYSQL_DB'] = 'test'
+mysql = MySQL(app)
+
 
 @app.route('/')
-def Welcome():
-    return 'Welcome Mybaseball!'
+def welcome():
+    cur = mysql.connection.cursor()
+    cur.execute('''SELECT * FROM book''')
+    result = cur.fetchall()
+    return str(result)
 
-if __name__ == '__main__' :
-    app.run(host = '0.0.0.0', port = 80)
-
+if __name__ == '__main__':
+    app.debug = True
+    app.run(host='0.0.0.0', port=80)
